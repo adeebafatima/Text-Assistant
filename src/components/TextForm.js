e59077a0a046bd1012f4485b9a05bd7f94217ab3
Adeeba Fatima <adeebafatima1@gmail.com>
2022-01-06 20:27:14 +0530

    toLowercase concept, Summary, Preview added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here");
+    const stopwords = [["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]];
+
+  const [text, setText] = useState("");
 
   const handleUpperCaseClick = () => {
     let upperCaseText = text.toUpperCase(); // "text"'s current state will be taken and converted to uppercase
@@ -13,8 +15,20 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleLowerCaseClick = () => {
+    let lowerCaseText = text.toLowerCase(); // "text"'s current state will be taken and converted to uppercase
+    setText(lowerCaseText); // New "text" state is upper case text
+  };
+
+  
+  const handleRemoveStopWordsClick = () => {
+    let withoutStopwordsText = text.replace(stopwords,""); // "text"'s current state will be taken and converted to uppercase
+    setText(withoutStopwordsText); // New "text" state is upper case text
+  };
+
   return (
-    <div>
+      <>
+    <div className="container">
       <h2>{props.boxHeading}</h2>
       <div className="mb-3">
         <textarea
@@ -25,10 +39,23 @@ export default function TextForm(props) {
           rows="10"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperCaseClick}>
+      <button className="btn btn-primary mx-3" onClick={handleUpperCaseClick}>
         Convert to Uppercase
       </button>
+      <button className="btn btn-primary mx-3" onClick={handleLowerCaseClick}>
+        Convert to Lowercase
+      </button>
     </div>
+
+    <div className="container my-3">
+        <h3>Your text summary</h3>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+
+    </div>
+    </>
   );
 }
 
