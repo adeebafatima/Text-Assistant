0df2a61ac3333e6a8d9d8022e2f6f0699103b2a3
Adeeba Fatima <adeebafatima1@gmail.com>
2022-01-07 00:15:22 +0530

    Dark Mode implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import "./App.css";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#2d2c31";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-      <Navbar title="Text Assistant" />
+      <Navbar title="Text Assistant" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <TextForm boxHeading="Enter the text to analyze" />
+        <TextForm boxHeading="Enter the text to analyze" mode={mode} />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function TextForm(props) {
-    const stopwords = [["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]];
-
   const [text, setText] = useState("");
 
   const handleUpperCaseClick = () => {
@@ -20,41 +18,48 @@ export default function TextForm(props) {
     setText(lowerCaseText); // New "text" state is upper case text
   };
 
-  
-  const handleRemoveStopWordsClick = () => {
-    let withoutStopwordsText = text.replace(stopwords,""); // "text"'s current state will be taken and converted to uppercase
-    setText(withoutStopwordsText); // New "text" state is upper case text
-  };
-
   return (
-      <>
-    <div className="container">
-      <h2>{props.boxHeading}</h2>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text} // Value must be assigned with text. So as "text" changes, it will be reflected.
-          onChange={handleOnChange} // In order to make text area editable,onChange event listner continously represent current "text" state
-          id="myBox"
-          rows="10"
-        ></textarea>
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <h2 style={{ color: props.mode === "light" ? "black" : "white" }}>
+          {props.boxHeading}
+        </h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text} // Value must be assigned with text. So as "text" changes, it will be reflected.
+            onChange={handleOnChange} // In order to make text area editable,onChange event listner continously represent current "text" state
+            id="myBox"
+            rows="10"
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "#2d2c31",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick={handleUpperCaseClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleLowerCaseClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary mx-3" onClick={handleUpperCaseClick}>
-        Convert to Uppercase
-      </button>
-      <button className="btn btn-primary mx-3" onClick={handleLowerCaseClick}>
-        Convert to Lowercase
-      </button>
-    </div>
 
-    <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
         <h3>Your text summary</h3>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <p>{0.008 * text.split(" ").length} minutes read</p>
         <h3>Preview</h3>
-        <p>{text}</p>
-
-    </div>
+        <p>{text.length > 0 ? text : "Enter text to preview"}</p>
+      </div>
     </>
   );
 }
